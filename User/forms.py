fa9ecbcced755fa3800d94f6af3534c0ff2c4c68
SaidParaBellum <asaidansor@gmail.com>
2024-08-17 17:38:15 +0500

    gtg1

--- User/forms.py
+++ User/forms.py
@@ -1,17 +1,24 @@
+
+
+
+
 from django import forms
-from django.contrib.auth.models import User
+
+from User.models import User
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ['first_name', 'username', 'password']
+        fields = ['username', 'email', 'password']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
 
-        password = self.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        return user
+        if password and password_confirmation and password != password_confirmation:
+            self.add_error('password_confirmation', "Passwords don't match")
--- User/urls.py
+++ User/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from User.views import UserLoginView
+from User.views import UserLoginView, RegisterView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(),name='login'),
     path('logout/', LogoutView.as_view(next_page='login'),name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 
 ]  +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
